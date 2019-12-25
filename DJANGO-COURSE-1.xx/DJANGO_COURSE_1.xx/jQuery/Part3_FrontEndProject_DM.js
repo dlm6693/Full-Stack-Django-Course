fa1e86585b9a7f7f21fe15a1aa992fbbac2cb997
David Miller <dlm6693@gmail.com>
2019-12-25 12:19:29 -0500

    connect 4 project progress

--- DJANGO-COURSE-1.xx/DJANGO_COURSE_1.xx/jQuery/Part3_FrontEndProject_DM.js
+++ DJANGO-COURSE-1.xx/DJANGO_COURSE_1.xx/jQuery/Part3_FrontEndProject_DM.js
@@ -4,6 +4,8 @@ $.fn.extend({
     }
 });
 
+var row = $('tr').eq(-1)
+
 player1 = prompt("Player One: Enter Your Name, you will be Blue")
 player2 = prompt("Player Two: Enter Your Name, you will be Red")
 player1_text = player1 + ": it is your turn, please pick a column to drop your blue chip." 
@@ -15,12 +17,33 @@ $('button').click(function(){
     $('h3').toggleText(player2_text, player1_text)
 })
 
-function turnRed(object){
-    return object.css('background', 'red');
-}
+$('td').click(move)
 
-function turnBlue(object){
-    return object.css('background', 'blue');
-}
 
-$('button').eq(-1).toggle(function(){this.css('background', 'blue')}, function(){this.css('background', 'red')});
+function move(){
+    var idx = $(this).index();
+    var box = row.children().eq(idx)
+    var button = box.children()
+    if (["rgb(255, 0, 0)", "rgb(0, 0, 255)"].includes(button.css('background-color'))) {
+        changerow('up')
+        if($('h3').text() === player2_text){
+            button.css('background-color', 'blue');
+        } else {
+            button.css('background-color', 'red');
+        };
+    } else if ($('h3').text() === player2_text) {
+        row = $('tr').eq(-1)
+        button.css('background-color', 'blue');
+    } else {
+        row = $('tr').eq(-1)
+        button.css('background-color', 'red');
+    }
+}
+function changerow(direction){
+    if (direction === 'up'){
+        row = row.prev()
+    } else if(direction === 'down'){
+        row = row.next()
+    }
+    
+}
