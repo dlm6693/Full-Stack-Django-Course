a1c3be2614fb2f0335c0f2cfe3222880be13f995
David Miller <dlm6693@gmail.com>
2019-12-31 13:30:40 -0500

    started django section

---
+++ mypython/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dlm66\onedrive\projects\full-stack-django-course\mypython\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
