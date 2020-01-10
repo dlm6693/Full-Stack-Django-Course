c835e5d26b5a660c3c3cb7dc7b81d260bf78000b
David Miller <dlm6693@gmail.com>
2020-01-10 13:30:56 -0500

    post templates done

--- django_stuff/simple_social/posts/models.py
+++ django_stuff/simple_social/posts/models.py
@@ -10,11 +10,11 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Post(models.Model):
-    user = models.ForeignKey(User, related_name='posts')
+    user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
     message = models.TextField()
     message_html = models.TextField(editable=False)
-    group = models.ForeignKey(Group, related_name='posts', null=True, blank=True)
+    group = models.ForeignKey(Group, related_name='posts', null=True, blank=True, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.message
@@ -24,7 +24,7 @@ class Post(models.Model):
         super().save(*args, **kwargs)
         
     def get_absolute_url(self):
-        return reverse('posts:single', kwargs={'username':self.user.username}, 'pk':self.pk)
+        return reverse('posts:single', kwargs={'username':self.user.username, 'pk':self.pk})
     
     class Meta:
         ordering = ['-created_at']
